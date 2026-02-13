a7d6940d81ee101baa91011d262da5ac230b8f55
Mahibalan-s <149420781+Mahibalan-s@users.noreply.github.com>
2026-02-13 17:34:00 +0530

    Update projectroutes.js

--- server/routes/projectroutes.js
+++ server/routes/projectroutes.js
@@ -4,7 +4,7 @@ const {
   getProjects,
   createProject,
   deleteProject,
-} = require("./controllers/projectcontroller");
+} = require("../controllers/projectcontroller");
 
 router.get("/", getProjects);
 router.post("/", createProject);
