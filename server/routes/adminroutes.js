7ad2a3110954403abc548cc720edaa51ecc68fcc
Mahibalan-s <149420781+Mahibalan-s@users.noreply.github.com>
2026-02-13 18:02:07 +0530

    Update adminroutes.js

--- server/routes/adminroutes.js
+++ server/routes/adminroutes.js
@@ -1,25 +1,7 @@
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-const Admin = require("../models/Admin");
+const express = require("express");
+const router = express.Router();
+const { login } = require("../controllers/admincontroller");
 
-const login = async (req, res) => {
-  try {
-    const { email, password } = req.body;
+router.post("/login", login);
 
-    const admin = await Admin.findOne({ email });
-    if (!admin) return res.status(400).json({ error: "Invalid credentials" });
-
-    const isMatch = await bcrypt.compare(password, admin.password);
-    if (!isMatch) return res.status(400).json({ error: "Invalid credentials" });
-
-    const token = jwt.sign({ id: admin._id }, "secretkey", {
-      expiresIn: "1d",
-    });
-
-    res.json({ token });
-  } catch (error) {
-    res.status(500).json({ error: "Server error" });
-  }
-};
-
-module.exports = { login };
+module.exports = router;
