460921d81aea8f9be1fa492ae310874ce5146e7d
Mahibalan-s <149420781+Mahibalan-s@users.noreply.github.com>
2026-02-13 18:03:20 +0530

    Update contactroutes.js

--- server/routes/contactroutes.js
+++ server/routes/contactroutes.js
@@ -1,13 +1,10 @@
 const express = require("express");
-
 const router = express.Router();
-const { createMessage,getMessages } = require("../controllers/contactcontroller");
+
+const { createMessage, getMessages } = require("../controllers/contactcontroller");
 const protect = require("../middleware/auth");
-router.get("/", protect, getMessages);
 
+router.get("/", protect, getMessages);
 router.post("/", createMessage);
-
-router.get("/", protect, getMessages);
-
 
 module.exports = router;
