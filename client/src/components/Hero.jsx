12648d3f33633316997a2f0e1f1848d000e3d074
Mahibalan-s <149420781+Mahibalan-s@users.noreply.github.com>
2026-02-15 08:03:54 +0530

    Update Hero.jsx

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -26,7 +26,7 @@ function Hero() {
           </div>
 
           <a
-            href="../public/Mahibalan_resume.pdf"
+            href="/Mahibalan_resume.pdf"
             target="_blank"
             rel="noopener noreferrer"
             className="resume-btn"
